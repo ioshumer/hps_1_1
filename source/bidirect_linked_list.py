1359383c7e1738f20367e92879da38c76b1c23bd
IO Shumer <ioshumer@gmail.com>
2023-03-28 14:12:57 +0300

    #Task 2: Bidirectional Linked List. insert, add_in_head, clean + tests

--- source/bidirect_linked_list.py
+++ source/bidirect_linked_list.py
@@ -1,8 +1,24 @@
+from typing import List
+
+
 class Node:
     def __init__(self, v):
         self.value = v
         self.prev = None
         self.next = None
+
+    def __str__(self):
+        template = "[Node ({node_id}): value={value}, prev={prev}, next={next}]"
+        string_representation = template.format(
+            node_id=id(self),
+            value=f"{self.value}",
+            prev=f"{self.prev.value}|{id(self.prev)}" if self.prev is not None else "None",
+            next=f"{self.next.value}|{id(self.next)}" if self.next is not None else "None"
+        )
+        return string_representation
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class LinkedList2:
@@ -65,7 +81,7 @@ class LinkedList2:
             node = node.next
 
     def clean(self):
-        pass  # здесь будет ваш код
+        self.head = self.tail = None
 
     def len(self):
         ctr = 0
@@ -76,10 +92,34 @@ class LinkedList2:
         return ctr
 
     def insert(self, afterNode, newNode):
-        pass  # здесь будет ваш код
+        if afterNode is None and self.len() == 0:
+            self.add_in_head(newNode)
+        elif afterNode is None or afterNode == self.tail:
+            self.add_in_tail(newNode)
+        else:
+            curr_node = self.head
+            while curr_node is not None:
+                if curr_node == afterNode:
+                    next_of_curr = curr_node.next
+                    newNode.prev = curr_node
+                    newNode.next = next_of_curr
+                    next_of_curr.prev = newNode
+                    curr_node.next = newNode
+                    break
+                curr_node = curr_node.next
 
-    def add_in_head(self, newNode):
-        pass  # здесь будет ваш код
+    def add_in_head(self, newNode: Node):
+        if self.head is None:
+            newNode.prev = None
+            newNode.next = None
+            self.head = newNode
+            self.tail = newNode
+            return None
+
+        curr_head = self.head
+        curr_head.prev = newNode
+        newNode.next = curr_head
+        self.head = newNode
 
     def as_list(self):
         node_values = []
@@ -89,8 +129,17 @@ class LinkedList2:
             node = node.next
         return node_values
 
+    def get_node_by_index(self, index):
+        ctr = 0
+        node = self.head
+        while node is not None:
+            if ctr == index:
+                return node
+            ctr += 1
+            node = node.next
 
-def create_bidirect_linked_list(list_of_values):
+
+def create_from_values(list_of_values):
     linked_list = LinkedList2()
 
     values_amount = len(list_of_values)
@@ -106,5 +155,31 @@ def create_bidirect_linked_list(list_of_values):
         value = list_of_values[value_index]
         new_node = Node(value)
         linked_list.add_in_tail(new_node)
+
+    return linked_list
+
+
+def create_from_nodes(list_of_nodes: List[Node]):
+    linked_list = LinkedList2()
+
+    values_amount = len(list_of_nodes)
+    if values_amount == 0:
+        return linked_list
+
+    if values_amount == 1:
+        linked_list.head = list_of_nodes[0]
+        linked_list.tail = list_of_nodes[0]
+        return linked_list
+
+    prev_node = list_of_nodes[0]
+    linked_list.head = prev_node
+
+    for node_idx in range(1, values_amount):
+        curr_node = list_of_nodes[node_idx]
+        prev_node.next = curr_node
+        curr_node.prev = prev_node
+        prev_node = curr_node
+
+    linked_list.tail = curr_node
 
     return linked_list
--- tests/test_bidirect_linked_list.py
+++ tests/test_bidirect_linked_list.py
@@ -1,32 +1,58 @@
+from typing import List
+
 import pytest
 
-from source.bidirect_linked_list import create_bidirect_linked_list
+from source.bidirect_linked_list import create_from_values, Node, create_from_nodes
 
 
 def test_list_creation():
     values = []
-    linked_list = create_bidirect_linked_list(values)
+    linked_list = create_from_values(values)
     assert values == linked_list.as_list()
     assert linked_list.head is None
     assert linked_list.tail is None
 
     values = [1]
-    linked_list = create_bidirect_linked_list(values)
+    linked_list = create_from_values(values)
     assert values == linked_list.as_list()
     assert linked_list.head.value == values[0]
     assert linked_list.tail.value == values[0]
 
     values = [1, 2]
-    linked_list = create_bidirect_linked_list(values)
+    linked_list = create_from_values(values)
     assert values == linked_list.as_list()
     assert linked_list.head.value == values[0]
     assert linked_list.tail.value == values[-1]
 
     values = [1, 3, 5, 13, 33, 55]
-    linked_list = create_bidirect_linked_list(values)
+    linked_list = create_from_values(values)
     assert values == linked_list.as_list()
     assert linked_list.head.value == values[0]
     assert linked_list.tail.value == values[-1]
+
+
+@pytest.mark.parametrize(
+    ('list_of_nodes', 'as_list', 'head_value', 'tail_value'),
+    [
+        ([], [], None, None),
+        ([Node(1)], [1], 1, 1),
+        ([Node(1), Node(2)], [1, 2], 1, 2),
+        ([Node(1), Node(2), Node(3)], [1, 2, 3], 1, 3),
+    ]
+)
+def test_creation_by_nodes(list_of_nodes, as_list, head_value, tail_value):
+    linked_list = create_from_nodes(list_of_nodes)
+    assert linked_list.as_list() == as_list
+
+    try:
+        assert linked_list.head.value == head_value
+    except AttributeError:
+        assert linked_list.head == head_value
+
+    try:
+        assert linked_list.tail.value == tail_value
+    except AttributeError:
+        assert linked_list.head == tail_value
 
 
 @pytest.mark.parametrize(
@@ -41,12 +67,32 @@ def test_list_creation():
     ]
 )
 def test_find(values, finding_value, result):
-    linked_list = create_bidirect_linked_list(values)
+    linked_list = create_from_values(values)
     found_node = linked_list.find(finding_value)
     if found_node is None:
         assert result is None
     else:
         assert found_node.value == result
+
+
+@pytest.mark.parametrize(
+    ('list_of_nodes', 'node_index', 'node_value'),
+    [
+        ([], 1, None),
+        ([Node(1)], 0, 1),
+        ([Node(1), Node(2)], 0, 1),
+        ([Node(1), Node(2)], 1, 2),
+        ([Node(1), Node(2), Node(3)], 1, 2),
+        ([Node(1), Node(2), Node(3)], 2, 3)
+    ]
+)
+def test_getting_by_node_index(list_of_nodes, node_index, node_value):
+    linked_list = create_from_nodes(list_of_nodes)
+    node = linked_list.get_node_by_index(node_index)
+    try:
+        assert node.value == node_value
+    except AttributeError:
+        assert node == node_value
 
 
 @pytest.mark.parametrize(
@@ -62,7 +108,7 @@ def test_find(values, finding_value, result):
     ]
 )
 def test_find_all(values, finding_value, result):
-    linked_list = create_bidirect_linked_list(values)
+    linked_list = create_from_values(values)
     found_result = [node.value for node in linked_list.find_all(finding_value)]
     assert found_result == result
 
@@ -91,7 +137,7 @@ def test_find_all(values, finding_value, result):
     ]
 )
 def test_delete(linked_list_values, value_to_delete, all_, as_list, tail_value, head_value, length):
-    linked_list = create_bidirect_linked_list(linked_list_values)
+    linked_list = create_from_values(linked_list_values)
     linked_list.delete(value_to_delete, all=all_)
     assert linked_list.as_list() == as_list
     if linked_list.head is not None:
@@ -103,3 +149,79 @@ def test_delete(linked_list_values, value_to_delete, all_, as_list, tail_value, 
     else:
         assert linked_list.tail is tail_value
     assert linked_list.len() == length
+
+
+@pytest.mark.parametrize(
+    ('list_of_nodes', 'new_node', 'head_value', 'tail_value', 'as_list'),
+    [
+        ([], Node(1), 1, 1, [1]),
+        ([Node(2)], Node(1), 1, 2, [1, 2]),
+        ([Node(2), Node(3)], Node(1), 1, 3, [1, 2, 3]),
+    ]
+)
+def test_add_in_head(list_of_nodes, new_node, head_value, tail_value, as_list):
+    linked_list = create_from_nodes(list_of_nodes)
+    linked_list.add_in_head(new_node)
+    assert linked_list.head.value == head_value
+    assert linked_list.tail.value == tail_value
+    assert linked_list.as_list() == as_list
+
+
+@pytest.mark.parametrize(
+    ('linked_list_values', 'list_type', 'after_node', 'new_node', 'head_value', 'tail_value', 'as_list', 'length'),
+    [
+        ([], 'int', None, Node(13), 13, 13, [13], 1),
+        ([Node(1)], 'nodes', None, Node(13), 1, 13, [1, 13], 2),
+    ]
+)
+def test_insert_empty(linked_list_values, list_type, after_node, new_node, head_value, tail_value, as_list, length):
+    if list_type == 'int':
+        linked_list = create_from_values(linked_list_values)
+    elif list_type == 'nodes':
+        linked_list = create_from_nodes(linked_list_values)
+    linked_list.insert(after_node, new_node)
+    assert linked_list.head.value == head_value
+    assert linked_list.tail.value == tail_value
+    assert linked_list.as_list() == as_list
+    assert linked_list.len() == length
+
+
+@pytest.mark.parametrize(
+    ('linked_list_values', 'after_node_idx', 'new_node', 'head_value', 'tail_value', 'as_list', 'length'),
+    [
+        ([], None, Node(5), 5, 5, [5], 1),
+        ([Node(3)], 0, Node(5), 3, 5, [3, 5], 2),
+        ([Node(3), Node(5)], 0, Node(4), 3, 5, [3, 4, 5], 3),
+    ]
+)
+def test_insert_positioned(
+        linked_list_values: List[Node], after_node_idx, new_node: Node, head_value, tail_value, as_list, length
+):
+    linked_list = create_from_nodes(linked_list_values)
+    after_node = linked_list.get_node_by_index(after_node_idx) if after_node_idx is not None else None
+    next_of_after_node = after_node.next if after_node_idx is not None else None
+    linked_list.insert(after_node, new_node)
+    assert linked_list.head.value == head_value if linked_list.head is not None else linked_list.head == head_value
+    assert linked_list.tail.value == tail_value if linked_list.tail is not None else linked_list.tail == tail_value
+    if after_node:
+        assert after_node.next == new_node
+    if next_of_after_node:
+        assert next_of_after_node.prev == new_node
+    assert new_node.prev == after_node
+    assert new_node.next == next_of_after_node
+
+
+@pytest.mark.parametrize(
+    ('linked_list_values', 'as_list', 'length'),
+    [
+        ([], [], 0),
+        ([1], [], 0),
+        ([1, 2], [], 0),
+        ([1, 2, 3], [], 0),
+    ]
+)
+def test_clean(linked_list_values, as_list, length):
+    linked_list = create_from_values(linked_list_values)
+    linked_list.clean()
+    assert linked_list.as_list() == as_list
+    assert linked_list.len() == 0

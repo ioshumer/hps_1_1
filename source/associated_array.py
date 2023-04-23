2cb50a0771df61870452b52d18d2ecde29fe1901
IO Shumer <ioshumer@gmail.com>
2023-04-23 11:34:00 +0300

    #Task 9. Native Dict. Bugfix: hash_fun argument to str

--- source/associated_array.py
+++ source/associated_array.py
@@ -5,23 +5,30 @@ class NativeDictionary:
         self.values = [None] * self.size
 
     def hash_fun(self, key):
-        byte_string = key.encode()
+        byte_string = str(key).encode()
         hash_sum = sum(byte_string) % self.size
         return hash_sum
 
     def is_key(self, key):
+        if key is None:
+            return False
         idx_of_slot = self.hash_fun(key)
         if self.values[idx_of_slot] is not None:
             return True
         return False
 
     def put(self, key, value):
+        if type(key) is not str:
+            return None
+
         idx_of_key = self.hash_fun(key)
         idx_of_value = self.hash_fun(value)
         self.values[idx_of_key] = idx_of_value
         self.slots[idx_of_value] = value
 
     def get(self, key):
+        if key is None:
+            return None
         idx_of_key = self.hash_fun(key)
         if self.values[idx_of_key] is not None:
             idx_of_value = self.values[idx_of_key]
--- tests/test_associated_array.py
+++ tests/test_associated_array.py
@@ -6,7 +6,7 @@ from source.associated_array import NativeDictionary
 @pytest.mark.parametrize(
     ("size", "key", "value", "new_value"),
     [
-        (127, 'hello', 'world', 'amigo')
+        (127, 'hello', 'world', 'amigo'),
     ]
 )
 def test_native_dictonary(size, key, value, new_value):
@@ -26,3 +26,35 @@ def test_native_dictonary(size, key, value, new_value):
 
     assert nd.is_key('capibara') is False
     assert nd.get('capibara') is None
+
+
+def test_get():
+    key = "hello"
+    value = "world"
+
+    nd = NativeDictionary(17)
+    nd.put(key, value)
+    assert nd.get(key) == value
+    assert nd.get("non_existed") is None
+
+
+def test_is_key():
+    key = "hello"
+    value = "world"
+
+    nd = NativeDictionary(17)
+    nd.put(key, value)
+    assert nd.is_key(key) is True
+    assert nd.is_key("non_existed") is False
+
+@pytest.mark.parametrize(
+    ("size", "key", "value"),
+    [
+        (7, "a", 100),
+    ]
+)
+def test_put_on_new_key(size, key, value):
+    nd = NativeDictionary(size)
+    nd.put(key, value)
+
+    assert nd.get(key) == value

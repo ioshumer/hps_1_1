bd5659c3204193d018445a17f6d968a62fce9c0c
IO Shumer <ioshumer@gmail.com>
2023-04-23 14:20:14 +0300

    #Task 9. Native Dict. Refactoring: store keys as indexed values

--- source/associated_array.py
+++ source/associated_array.py
@@ -12,25 +12,22 @@ class NativeDictionary:
     def is_key(self, key):
         if key is None:
             return False
-        idx_of_slot = self.hash_fun(key)
-        if self.values[idx_of_slot] is not None:
-            return True
+        for item in self.values:
+            if item == key:
+                return True
         return False
 
     def put(self, key, value):
         if type(key) is not str:
             return None
-
-        idx_of_key = self.hash_fun(key)
         idx_of_value = self.hash_fun(value)
-        self.values[idx_of_key] = idx_of_value
+        self.values[idx_of_value] = key
         self.slots[idx_of_value] = value
 
     def get(self, key):
         if key is None:
             return None
-        idx_of_key = self.hash_fun(key)
-        if self.values[idx_of_key] is not None:
-            idx_of_value = self.values[idx_of_key]
-            return self.slots[idx_of_value]
+        for idx, item in enumerate(self.values):
+            if item == key:
+                return self.slots[idx]
         return None
